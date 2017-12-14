83936523f7a2e851bb3c09146d31cc52d1216cd2
DESKTOP-RI538U1\jeffe <jeffersonxplode@gmail.com>
2017-12-14 04:50:37 -0300

    Refactoring.

--- Source/FPSProject/FPSProjectGameMode.cpp
+++ Source/FPSProject/FPSProjectGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "FPSProjectGameMode.h"
 #include "FPSProjectHUD.h"
-#include "FPSProjectCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AFPSProjectGameMode::AFPSProjectGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
