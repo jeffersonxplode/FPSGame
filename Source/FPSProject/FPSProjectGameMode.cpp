1a227e3ff62c52b5063a6a602982e8e13e067f83
DESKTOP-RI538U1\jeffe <jeffersonxplode@gmail.com>
2018-02-16 01:40:19 -0300

    Fixing the Shoot Method.

--- Source/FPSProject/FPSProjectGameMode.cpp
+++ Source/FPSProject/FPSProjectGameMode.cpp
@@ -9,7 +9,7 @@ AFPSProjectGameMode::AFPSProjectGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Behaviour/Character"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Behaviour/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
